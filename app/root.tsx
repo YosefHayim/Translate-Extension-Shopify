a13b56d9d88fc15436eb19679c26263e07b294b0
yosefhayim <yosefisabag+03@gmail.com>
2025-06-25 10:00:38 +0300

    trying to init tailwind to the proejct

--- app/root.tsx
+++ app/root.tsx
@@ -1,11 +1,4 @@
-import {
-  Links,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-  useNavigation,
-} from "@remix-run/react";
+import { Links, Meta, Outlet, Scripts, ScrollRestoration, useNavigation } from "@remix-run/react";
 
 import type { LinksFunction } from "@remix-run/node";
 import styles from "./tailwind.css?url";
@@ -21,16 +14,12 @@ export default function App() {
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
         <link rel="preconnect" href="https://cdn.shopify.com/" />
-        <link
-          rel="stylesheet"
-          href="https://cdn.shopify.com/static/fonts/inter/v4/styles.css"
-        />
+        <link rel="stylesheet" href="https://cdn.shopify.com/static/fonts/inter/v4/styles.css" />
         <Meta />
         <Links />
       </head>
       <body>
         {isNavigating && <div>Loading...</div>}
-
         <Outlet />
         <ScrollRestoration />
         <Scripts />
